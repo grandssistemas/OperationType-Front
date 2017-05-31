abaf12ca45abf3d5f90837a40aef3928d59e3a4e
Luiz Augusto <luizaugusto@sgsistemas.com.br>
2017-05-31 15:37:23 -0300

    terminado tela de regra comercial

--- app/modules/businessrule/controllers/BusinessRuleStepTwoController.js
+++ app/modules/businessrule/controllers/BusinessRuleStepTwoController.js
@@ -14,11 +14,11 @@ angular.module('app.businessrule.controllers')
                   BusinessRuleService) {
 
 
-            $scope.operations = angular.copy(operations)
+            $scope.operations = angular.copy(operations);
 
             BusinessRuleService.getNew().then(function(response){
                 $scope.new = response.data;
-            })
+            });
 
             if (!$scope.operations || !$scope.operations.length) {
                 SweetAlert.swal({
@@ -88,54 +88,47 @@ angular.module('app.businessrule.controllers')
 
             $scope.back = function(){
                 $state.go('businessrule.stepone');
-            }
+            };
 
             $scope.generate = function(){
                 var selected = $scope.entry ? $scope.selectedEntryType : $scope.selectedParcelType;
                 var generated = [];
-                var singleParcel = [];
-                var manyParcels = [];
-
+                var parcels = [];
                 selected.forEach(function(payment){
-                    if (payment.parcelsCount > 0){
-                        
+                    var last = payment.parcelsCount;
+                    for (var i = 1; i <= last; i++){
+                        parcels[i] = parcels[i] || [];
+                        parcels[i].push(payment);
                     }
                 });
 
-
-
-                selected.forEach(function(current){
-                    for(var count = 1; count <= current.parcelsCount; count ++){
-                        var br = angular.copy($scope.new);
-                        br.maxDiscount = 100;
-                        br.discountType = 'PERCENTAGE';
-                        br.hasEntry = $scope.entry;
-                        br.minValue = 0;
-                        br.startDuration = (new Date());
-                        br.active = $scope.active;
-                        br.negotiationInterval = 0;
-                        if (br.hasEntry){
-                            br.entryValue = 1;
-                            br.entryType = 'PERCENTAGE'
-                            br.entryPaymentTypes = selected;
-                            br.parcelsCount = 0;
-                        } else {
-                            br.parcelsCount = count;
-                            br.parcelsPaymentTypes = selected;
-                            br.monthly = true;
-                        }
-                        generated.push(br);
+                for(var count = 1; count <= parcels.length -1 ; count ++){
+                    var br = angular.copy($scope.new);
+                    br.maxDiscount = 100;
+                    br.discountType = 'PERCENTAGE';
+                    br.hasEntry = $scope.entry;
+                    br.minValue = 0;
+                    br.startDuration = (new Date());
+                    br.active = $scope.active;
+                    br.negotiationInterval = 0;
+                    if (br.hasEntry) {
+                        br.entryValue = 1;
+                        br.entryType = 'PERCENTAGE';
+                        br.entryPaymentTypes = parcels[count];
+                        br.parcelsCount = 0;
+                    } else {
+                        br.entryValue = 0;
+                        br.parcelsCount = count;
+                        br.parcelsPaymentTypes = parcels[count];
+                        br.monthly = true;
                     }
+                    generated.push(br);
+                }
 
-
-                })
-                BusinessRuleService.saveWithOperationType($scope.operations, generated).then(function(response){
-                    console.log(response.data)
+                BusinessRuleService.saveWithOperationType($scope.operations, generated).then(function(){
+                    $state.go('businessrule.list');
                 });
-
-
-
-            }
+            };
 
             $scope.disableBtnSave = function(){
                 return ($scope.entry && !$scope.selectedEntryType.length) || ($scope.parcel && !$scope.selectedParcelType.length)
